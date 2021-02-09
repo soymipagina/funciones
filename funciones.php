82d392ff20cb9f6848fa2d3fc43adc414b9f7686
Luis Chacon <fchacon@soymipagina.com>
2021-02-08 23:18:54 -0600

    Se agregó código  nuevo para personalizar el logo

--- funciones.php
+++ funciones.php
@@ -39,3 +39,18 @@ function zoom_effect_remover() {
     
     add_action( 'wp_head', 'zoom_effect_remover' );
     add_action( 'admin_head', 'zoom_effect_remover' );
+
+    /*Nueva función para personalizar el logo*/
+    function my_login_logo() { ?>
+        <style type=”text/css”>
+        #login h1 a, .login h1 a {
+        background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/new-logo.png);
+        height:65px;
+        width:320px;
+        background-size: 320px 65px;
+        background-repeat: no-repeat;
+        padding-bottom: 30px;
+        }
+        </style>
+        <?php }
+        add_action( ‘login_enqueue_scripts’, ‘my_login_logo’ );        
